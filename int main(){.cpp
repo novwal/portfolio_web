cad836a2d56f454ef9e105e4cedb7c269d20afd9
novwal <novanugget64@gmail.com>
2022-11-09 14:23:30 +0700

    perbarui

---
+++ int main(){.cpp
@@ -0,0 +1,24 @@
+int main(){
+    int pil,batas;
+    cout<<"Masukan pilihan [1.Ganjil] ]2,Genap] : "
+cin>>pil;
+cout<<"Masukan Batas : ";
+cin>>batas;
+cout<<end1;
+
+//if (pil==1){
+    //cout<<"Deret Bilangan Ganjil"<<endL;
+    for(int i=1;i<=batas;++){
+        if(1%2=0){
+            cout<<i<<"ganjil ";
+        }
+        else
+        {
+            cout<<i<<"genap\n ";
+
+        }
+    }
+//}else {
+//  cout<<"Deret Bilangan Genap"<<endL;
+
+}
